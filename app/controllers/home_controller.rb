b48e350b6e837002384640100521346318d2fd49
Tanner Hartwig <tanner.p.hartwig47@gmail.com>
2015-07-21 19:46:09 -0500

    Refactored code. Works with or without JS.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,37 @@ class HomeController < ApplicationController
   end
 
   def show
+    @has_valid_dates = true
     @sundays = 0
-    @start_date = Date.parse(params[:start_date])
-    @end_date = Date.parse(params[:end_date])
 
-    # I feel like this is cheating... I love ruby.
-    @start_date.upto(@end_date) do |date|
-      if date.sunday?
-        @sundays += 1
+    # Check if dates were input
+    if params[:start_date].empty? || params[:end_date].empty?
+      flash.now[:alert] = "Must enter a start and end date."
+      @has_valid_dates = false
+    end
+
+    # Assign dates
+    if @has_valid_dates
+      @start_date = Date.parse(params[:start_date])
+      @end_date = Date.parse(params[:end_date])
+    end
+
+    # Check if start_date is before end_date
+    if @has_valid_dates && (@start_date > @end_date)
+      flash.now[:alert] = "Start date must come before end date."
+      @has_valid_dates = false
+    end
+
+    # This feels like cheating... I love Ruby.
+    if @has_valid_dates
+      @start_date.upto(@end_date) do |date|
+        if date.sunday? then @sundays += 1 end
       end
     end
+
+    unless request.xhr?
+      render 'index' if !@has_valid_dates
+    end
   end
-  
+
 end
